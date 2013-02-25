ecec1812d19e021252a665b2ebb21d9864ce092a
Jonathan Barnett @indieisaconcept <me@indieisaconcept.com>
2013-02-26 00:49:14 +1100

    Auto create fixes

--- app/data/packages.sql
+++ app/data/packages.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS "packages" (
     "id"            INTEGER PRIMARY KEY,
     "name"          VARCHAR NOT NULL UNIQUE, 
     "url"           VARCHAR NOT NULL UNIQUE,
-    "hits"          INTEGER NOT NULL DEFAULT 0,
+    "hits"          INTEGER NOT NULL DEFAULT 1,
     "created_at"    DATETIME NOT NULL DEFAULT (DATETIME("now", "localtime")),
     "updated_at"    DATETIME NOT NULL DEFAULT (DATETIME("now", "localtime"))
     
